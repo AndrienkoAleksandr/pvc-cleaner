f4ce4961dda92ce0bec7da4549c05d2d69bfe530
Oleksandr Andriienko <oandriie@redhat.com>
2022-02-22 01:49:09 +0200

    Split rest logic.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- main.go
+++ main.go
@@ -1,22 +1,15 @@
 package main
 
 import (
-	"context"
 	"os"
 	"flag"
-	"fmt"
 	"log"
-
 	"path/filepath"
-
-	"github.com/AndrienkoAleksandr/pvc-cleaner/pkg/model"
 	"github.com/AndrienkoAleksandr/pvc-cleaner/pkg/storage"
 	"github.com/AndrienkoAleksandr/pvc-cleaner/pkg/k8s"
+	"github.com/AndrienkoAleksandr/pvc-cleaner/pkg/restapi"
 	"github.com/gin-gonic/gin"
 	"github.com/tektoncd/pipeline/pkg/client/clientset/versioned"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/util/homedir"
@@ -45,12 +38,12 @@ func main() {
 		panic(err.Error())
 	}
 
-	cs, err := versioned.NewForConfig(config)
+	tknClientset, err := versioned.NewForConfig(config)
 	if err != nil {
 		log.Fatalf("failed to create pipeline clientset %s", err)
 	}
 	// todo set up current namespace....
-	pipelinesRunApi := cs.TektonV1beta1().PipelineRuns("default")
+	pipelinesRunApi := tknClientset.TektonV1beta1().PipelineRuns("default")
 
 	subPathStorage := storage.NewPVCSubPathsStorage()
 	if err := subPathStorage.Init(); err != nil {
@@ -59,134 +52,11 @@ func main() {
 
 	r := gin.Default()
 
-	r.POST("/pipeline-run", func(c *gin.Context) {
-		pipelineRunName := c.Request.FormValue("pipeline-run-name")
-		if pipelineRunName == "" {
-			c.JSON(400, gin.H{
-				"error": "Bad request",
-			})
-			return
-		}
+	r.POST("/pipeline-run", restapi.StorePVCSubPath(pipelinesRunApi, subPathStorage))
 
-		fmt.Println(pipelineRunName)
+	r.GET("/pipeline-run/list", restapi.GetAllPipelineWithPVCSubPath(subPathStorage))
 
-		pipelineRun, err := pipelinesRunApi.Get(context.TODO(), pipelineRunName, metav1.GetOptions{})
-		if err != nil {
-			c.AbortWithError(500, err)
-			return
-		}
-
-		var subPath string
-		for _, workspace := range pipelineRun.Spec.Workspaces {
-			if workspace.Name == "workspace" {
-				subPath = workspace.SubPath
-				break
-			}
-		}
-
-		if subPath != "" {
-			pvcSubPath := &model.PVCSubPath{PipelineRun: pipelineRunName, PVCSubPath: subPath}
-			if err := subPathStorage.AddPVCSubPath(pvcSubPath); err != nil {
-				c.AbortWithError(500, err)
-				return
-			}
-		}
-
-		fmt.Println("Got it!" + pipelineRun.Name)
-		c.JSON(201, gin.H{
-			"message": "server got pipeline run name " + pipelineRunName,
-		})
-	})
-
-	r.GET("/pipeline-run/list", func(c *gin.Context) {
-		subPaths, err := subPathStorage.GetAll()
-		if err != nil {
-			c.AbortWithError(500, err)
-			return
-		}
-		c.JSON(200, subPaths)
-	})
-
-	r.DELETE("/pipeline-run/list", func(c *gin.Context) {
-		subPaths, err := subPathStorage.GetAll()
-		if err != nil {
-			c.AbortWithError(500, err)
-			return
-		}
-
-		pvcToCleanUp := []*model.PVCSubPath{}
-		for _, pvcSubPath := range subPaths {
-			_, err := pipelinesRunApi.Get(context.TODO(), pvcSubPath.PipelineRun, metav1.GetOptions{})
-			if errors.IsNotFound(err) {
-				pvcToCleanUp = append(pvcToCleanUp, pvcSubPath)
-			}
-		}
-
-		fmt.Println(pvcToCleanUp)
-
-		if len(pvcToCleanUp) == 0 {
-			c.JSON(200, gin.H{"message:": "Nothing to delete"})
-			return
-		}
-
-		var delFoldersContentCmd string
-		var volumeMounts []corev1.VolumeMount
-		for _, pvc := range pvcToCleanUp {
-			delFoldersContentCmd += "cd /workspace/source/" + pvc.PVCSubPath + "; ls -A | xargs rm -rfv;"
-			volumeMounts = append(volumeMounts, corev1.VolumeMount{
-				Name:      "source",
-				MountPath: "/workspace/source/" + pvc.PVCSubPath,
-				SubPath: pvc.PVCSubPath,
-			})
-		}
-
-		deadline := int64(5400)
-		pvcCleanerPod := &corev1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "clean-pvc-pod",
-				Namespace: "default", // todo
-			},
-			Spec: corev1.PodSpec{
-				RestartPolicy:         "Never",
-				ActiveDeadlineSeconds: &deadline,
-				Containers: []corev1.Container{
-					{
-						Name: "pvc-cleaner",
-						Command: []string{
-							"/bin/bash",
-						},
-						TTY: true,
-						Args: []string{
-							"-c",
-							delFoldersContentCmd,
-						},
-						Image: "registry.access.redhat.com/ubi8/ubi",
-						VolumeMounts: volumeMounts,
-						WorkingDir: "/",
-					},
-				},
-				Volumes: []corev1.Volume{
-					{
-						Name: "source",
-						VolumeSource: corev1.VolumeSource{
-							PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
-								ClaimName: "app-studio-default-workspace",
-							},
-						},
-					},
-				},
-			},
-		}
-
-		// todo set up namespace
-		_, err = clientset.CoreV1().Pods("default").Create(context.TODO(), pvcCleanerPod, metav1.CreateOptions{})
-		if err != nil {
-			c.AbortWithError(500, err)
-			return
-		}
-
-		c.JSON(200, gin.H{"message:": "Executed delete pod to clean up pvc subfolders"})
-	})
+	r.DELETE("/pipeline-run/list", restapi.DeleteNotUsedSubPaths(pipelinesRunApi, subPathStorage, clientset))
 
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
